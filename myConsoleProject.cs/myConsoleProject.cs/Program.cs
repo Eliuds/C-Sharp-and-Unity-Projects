bf02e66c049ce26422243b4f744f624318a0db0f
Eliuds <cmeliud2003@gmail.com>
2022-02-07 14:00:04 -0500

    asd

---
+++ myConsoleProject.cs/myConsoleProject.cs/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace myConsoleProject.cs
+{
+    class Program
+    {
+        static void Main(Console.WriteLine("What is your name?");
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
