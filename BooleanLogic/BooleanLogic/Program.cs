5279009ce4f6f729d55f0f8ecf46e867122bd638
Eliuds <cmeliud2003@gmail.com>
2022-02-08 16:56:19 -0500

    assignment

---
+++ BooleanLogic/BooleanLogic/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace BooleanLogic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine(true && false);
+            //Console.WriteLine(true && true);
+            //Console.WriteLine(false && false);
+
+            //Console.WriteLine(true || true);
+            //Console.WriteLine(true || false);
+            //Console.WriteLine(false || false);
+
+            //Console.WriteLine(true == true);
+            //Console.WriteLine(true == false);
+            //Console.WriteLine(false == false);
+
+            //Console.WriteLine(true != true);
+            //Console.WriteLine(true != false);
+            //Console.WriteLine(false!= false);
+
+            //Console.WriteLine(true ^ true);
+            //Console.WriteLine(true ^ false);
+            //Console.WriteLine(false ^ false); // only if onw of them are true
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ approval program for car insurance/approval program for car insurance/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace approval_program_for_car_insurance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your age?");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Have you ever had a DUI, plz respond with true or false?");
+            bool DUI = Convert.ToBoolean(Console.ReadLine());
+
+            Console.WriteLine("How many speeding tickets do you have?");
+            int speedtickets = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("qualified");
+
+            bool qualified = (age > 15 && (DUI == false && (speedtickets < 3)));
+            Console.WriteLine(qualified);
+        }
+    }
+}
