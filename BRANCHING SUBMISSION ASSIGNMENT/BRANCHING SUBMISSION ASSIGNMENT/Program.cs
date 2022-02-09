637baabe06d9c4042a24f8d2527b118e1400c5c6
Eliuds <cmeliud2003@gmail.com>
2022-02-08 22:32:43 -0500

    assignment

---
+++ BRANCHING SUBMISSION ASSIGNMENT/BRANCHING SUBMISSION ASSIGNMENT/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace BRANCHING_SUBMISSION_ASSIGNMENT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Package Express. Please follow the instructions below.");
+
+            Console.WriteLine("Please enter package weight");
+            int Weight = Convert.ToInt32(Console.ReadLine());
+            if(Weight > 50)
+            {
+                Console.WriteLine("Package too heavy to be shipped via Package Express. Have a good day.");
+            }
+
+            Console.WriteLine("What is the width of your packag?");
+            int Width = Convert.ToInt32(Console.ReadLine());
+
+            if (Width > 50)
+            {
+                Console.WriteLine("Package too big to be shipped via Package Express.");
+            }
+
+            Console.WriteLine("What is the height of the package?");
+            int Height = Convert.ToInt32(Console.ReadLine());
+
+            if (Height > 50)
+            {
+                Console.WriteLine("Package too big to be shipped via Package Express.");
+            }
+
+            Console.WriteLine("What is the length of your package?");
+            int Length = Convert.ToInt32(Console.ReadLine());
+
+            if (Length> 50)
+            {
+                Console.WriteLine("Package too big to be shipped via Package Express.");
+            }
+
+            decimal quote = Height * Width * Length * Weight / 100;
+
+            Console.WriteLine("$" + quote);
+            Console.ReadLine();
+
+
+             
+        }
+    }
+}
---
+++ Branching/Branching/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace Branching
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your favorite number?");
+            int favnum = Convert.ToInt32(Console.ReadLine());
+
+            string result = favnum == 55 ? "You have an awesome favorite number." : "You do not have an awesome favorite number.";
+
+            Console.WriteLine(result);
+            Console.ReadLine();
+            //int roomTemp = 70;
+
+            //Console.WriteLine("Hi, what is your name?");
+            //string name = Console.ReadLine();
+
+            //Console.WriteLine("Hi, " + name + ", what is the temperature where you are?");
+            //    int currentTemp = Convert.ToInt32(Console.ReadLine());
+
+            //if(currentTemp == roomTemp)
+            //{
+            //    Console.WriteLine("it is exactly room temp.");
+            //}
+            //else if (currentTemp > roomTemp)
+            //{
+            //    Console.WriteLine("It is warmer than room temp");
+            //}
+            //else if (roomTemp > currentTemp)
+            //{
+            //    Console.WriteLine("It is colder than room temp.");
+            //}
+            //else
+            //{
+            //    Console.WriteLine(" uo-oh.... something went wrong.");
+            //}
+            //int currentTemp = 70;
+            //int roomTemp = 70;
+
+            //string comparisonResult = currentTemp == roomTemp ? "it is room temp." : "It is not room temp.";
+
+            //Console.WriteLine(comparisonResult);
+            //Console.ReadLine();
+            //if (currentTemp == roomTemp)
+            //{
+            //    Console.WriteLine("It is exactly room temp.");
+            //}
+            //else if (currentTemp > roomTemp)
+            //{
+            //    Console.WriteLine("It is warmer than room temp.");
+            //}
+            //else if (roomTemp > currentTemp)
+            //{
+            //    Console.WriteLine("Room temp is warmer then the current temperature.");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("It is not exactly room Temp.");
+            //}
+            Console.ReadLine();
+        }
+    }
+}
