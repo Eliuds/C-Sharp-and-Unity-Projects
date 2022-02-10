f54244e892a539484da90b55646d20de96c509fa
Eliuds <cmeliud2003@gmail.com>
2022-02-10 01:37:51 -0500

    ll

--- BRANCHING SUBMISSION ASSIGNMENT/BRANCHING SUBMISSION ASSIGNMENT/Program.cs
+++ BRANCHING SUBMISSION ASSIGNMENT/BRANCHING SUBMISSION ASSIGNMENT/Program.cs
@@ -10,35 +10,43 @@ namespace BRANCHING_SUBMISSION_ASSIGNMENT
 
             Console.WriteLine("Please enter package weight");
             int Weight = Convert.ToInt32(Console.ReadLine());
-            if(Weight > 50)
+            if (Weight > 50)
             {
                 Console.WriteLine("Package too heavy to be shipped via Package Express. Have a good day.");
             }
-
-            Console.WriteLine("What is the width of your packag?");
-            int Width = Convert.ToInt32(Console.ReadLine());
-
-            if (Width > 50)
+            else
             {
-                Console.WriteLine("Package too big to be shipped via Package Express.");
+                Console.WriteLine("What is the width of your packag?");
+                int Width = Convert.ToInt32(Console.ReadLine());
+
+                if (Width > 50)
+                {
+                    Console.WriteLine("Package too big to be shipped via Package Express.");
+                }
+                else
+                {
+                    Console.WriteLine("What is the height of the package?");
+                    int Height = Convert.ToInt32(Console.ReadLine());
+
+                    if (Height > 50)
+                    {
+                        Console.WriteLine("Package too big to be shipped via Package Express.");
+                    }
+
+                    else
+                    {
+
+
+                        Console.WriteLine("What is the length of your package?");
+                        int Length = Convert.ToInt32(Console.ReadLine());
+
+                        if (Length > 50)
+                        {
+                            Console.WriteLine("Package too big to be shipped via Package Express.");
+                        }
+                    }
+                }
             }
-
-            Console.WriteLine("What is the height of the package?");
-            int Height = Convert.ToInt32(Console.ReadLine());
-
-            if (Height > 50)
-            {
-                Console.WriteLine("Package too big to be shipped via Package Express.");
-            }
-
-            Console.WriteLine("What is the length of your package?");
-            int Length = Convert.ToInt32(Console.ReadLine());
-
-            if (Length> 50)
-            {
-                Console.WriteLine("Package too big to be shipped via Package Express.");
-            }
-
             decimal quote = Height * Width * Length * Weight / 100;
 
             Console.WriteLine("$" + quote);
