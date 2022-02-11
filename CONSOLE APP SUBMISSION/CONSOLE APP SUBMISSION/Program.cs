4ced5882690e92c8faddbf30f1317246144e3e13
Eliuds <cmeliud2003@gmail.com>
2022-02-11 14:21:48 -0500

    asd

--- CONSOLE APP SUBMISSION/CONSOLE APP SUBMISSION/Program.cs
+++ CONSOLE APP SUBMISSION/CONSOLE APP SUBMISSION/Program.cs
@@ -6,52 +6,52 @@ namespace CONSOLE_APP_SUBMISSION
     {
         static void Main(string[] args)
         {
-            //Console.WriteLine("Guess my age.");
-            //int number = Convert.ToInt32(Console.ReadLine());
-            //bool age = number == 19;
-            //do
-            //{
-            //    switch (number)
-            //    {
-            //        case 18:
-            //            Console.WriteLine("Oh so close, but nope");
-            //            Console.WriteLine("Guess my age.");
-            //            number = Convert.ToInt32(Console.ReadLine());
-            //            break;
+            Console.WriteLine("Guess my age.");
+            int number = Convert.ToInt32(Console.ReadLine());
+            bool age = number == 19;
+            do
+            {
+                switch (number)
+                {
+                    case 18:
+                        Console.WriteLine("Oh so close, but nope");
+                        Console.WriteLine("Guess my age.");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
 
-            //        case 30:
-            //            Console.WriteLine("Im not there yet");
-            //            Console.WriteLine("Guess my age.");
-            //            number = Convert.ToInt32(Console.ReadLine());
-            //            break;
+                    case 30:
+                        Console.WriteLine("Im not there yet");
+                        Console.WriteLine("Guess my age.");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
 
-            //        case 20:
-            //            Console.WriteLine("Oh so close, but nope");
-            //            Console.WriteLine("Guess my age.");
-            //            number = Convert.ToInt32(Console.ReadLine());
-            //            break;
+                    case 20:
+                        Console.WriteLine("Oh so close, but nope");
+                        Console.WriteLine("Guess my age.");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
 
-            //        case 19:
-            //            Console.WriteLine("Thats Correct!");
-
-
-            //            age = true;
-            //            break;
-
-            //        default:
-
-            //            Console.WriteLine("No thats not my age");
-            //            Console.WriteLine("Guess my age.");
-            //            number = Convert.ToInt32(Console.ReadLine());
-            //            break;
+                    case 19:
+                        Console.WriteLine("Thats Correct!");
 
 
-            //    }
-            //}
-            //while (!age);
+                        age = true;
+                        break;
+
+                    default:
+
+                        Console.WriteLine("No thats not my age");
+                        Console.WriteLine("Guess my age.");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
 
 
-            //Console.Read();
+                }
+            }
+            while (!age);
+
+
+            
 
 
             Console.WriteLine("Whats your favorite number?");
