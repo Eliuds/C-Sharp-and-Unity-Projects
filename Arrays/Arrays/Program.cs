3b3dc22e2d5cf430e1c1a939b30e534cfb37c244
Eliuds <cmeliud2003@gmail.com>
2022-02-12 23:00:05 -0500

    asde

--- Arrays/Arrays/Program.cs
+++ Arrays/Arrays/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using System.Collections.Generic;
+using System;
+
 
 namespace Arrays
 {
@@ -6,17 +8,28 @@ namespace Arrays
     {
         static void Main()
         {
-            string[] StringArray = new string[] {"Anto", "Nuny", "jay", "Yilmar" };
-            Console.ReadLine();
+            string[] StringArray = new string[] { "Anto", "Nuny", "jay", "Yilmar" };
+            Console.WriteLine(StringArray);
+            int index = Convert.ToInt32(Console.ReadLine());
 
-            int[] numArray = { 1, 2, 3, 4, 5, 6,  7 };
-            Console.WriteLine(numArray[6]);
-            Console.ReadLine();
+            (int i = 0; i < StringArray.Lenth; i++) 
+            }
 
+                if (index == i)
+                {
+                    Console.WriteLine(StringArrray[i]);
+                }
+}
+
+
+            int[] numArray = { 1, 2, 3, 4, 5, 6, 7 };
+            Console.WriteLine();
+            Console.ReadLine();
+        
             List<string> stringList = new List<string>();
             stringList.Add("Howdy");
-            stringList.add("Hello");
-            stringList.ass("Hi");
+            stringList.Add("Hello");
+            stringList.Add("Hi");
         }
     }
 }
