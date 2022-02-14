86abfeb644446e6cc925fad99f0d234cc3cd3dc8
Eliuds <cmeliud2003@gmail.com>
2022-02-14 14:55:27 -0500

    asd

--- Arrays/Arrays/Program.cs
+++ Arrays/Arrays/Program.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections.Generic;/// need to insert while having a list
 using System;
 
 
@@ -8,28 +8,59 @@ namespace Arrays
     {
         static void Main()
         {
-            string[] StringArray = new string[] { "Anto", "Nuny", "jay", "Yilmar" };
-            Console.WriteLine(StringArray);
-            int index = Convert.ToInt32(Console.ReadLine());
-
-            (int i = 0; i < StringArray.Lenth; i++) 
-            }
+            string[] stringarray = new string[] { "anto", "nuny", "jay", "yilmar" };// creating a array of strings.
+            Console.WriteLine("pick a number between 0 and 3 to get a name.");// telling the user to pick between one of these numbers program wont work if they pick anything else.
+            int index = Convert.ToInt32(Console.ReadLine()); // to get the user input
+            for (int i = 0; i < stringarray.Length; i++)// this is saying as long as i is less then the length of the string array keep the program going in this case its 4 so you can only put up to 3, 0,1,2,3.
+            {
 
                 if (index == i)
                 {
-                    Console.WriteLine(StringArrray[i]);
+                    Console.WriteLine(stringarray[i]);
                 }
-}
 
 
-            int[] numArray = { 1, 2, 3, 4, 5, 6, 7 };
-            Console.WriteLine();
-            Console.ReadLine();
-        
-            List<string> stringList = new List<string>();
-            stringList.Add("Howdy");
-            stringList.Add("Hello");
-            stringList.Add("Hi");
+            //}
+            //foreach (var str in stringarray)
+            //{
+            //    Console.WriteLine(str);// another way to create the list
+            }
+
+
+            int[] numArray = new int[] { 10, 32, 32, 45, 54, 65, 234 };// creating a array of numbers
+            try
+            {
+                Console.WriteLine("now pick a number between 0 and 6 to get a number");// asking user for number between 0 and 6 anything else and it wont work
+                int index2 = Convert.ToInt32(Console.ReadLine());// to get user input
+                Console.WriteLine(numArray[index2]);
+               
+            }
+
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+
+            List<string> stringList = new List<string>();// making a list of strings
+            stringList.Add("Howdy");//adding the word howdy it now has the index value 0
+            stringList.Add("Hello");//adding the word hello it now has the index value 1
+            stringList.Add("Hi");//adding the word hi it now has the index value 2
+            Console.WriteLine("Pick a number between 0 and 2 to get a greeting");// telling user to pick a number between 0 and 2, anthing else the program wont work
+            int index3 = Convert.ToInt32(Console.ReadLine());// getting user input
+
+            for (int y = 0; y < stringList.Count; y++)// the count pretty much means the same as length but its what is used for lists.
+            {
+
+                if (index3 == y)
+                {
+                    Console.WriteLine(stringList[y]);
+                }
+
+
+            }// can also use the try method used for the numarray for this it would be easier.
         }
     }
 }
+            
+         
+
