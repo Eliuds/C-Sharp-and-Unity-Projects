5d1c4a612a2fcf30683984f3cfca43b7932ce288
Eliuds <cmeliud2003@gmail.com>
2022-02-14 15:19:14 -0500

    asd

--- Arrays/Arrays/Program.cs
+++ Arrays/Arrays/Program.cs
@@ -18,17 +18,21 @@ namespace Arrays
                 {
                     Console.WriteLine(stringarray[i]);
                 }
+                else
+                {
+                    Console.WriteLine("must be a number between 1 and 3");
+                }
 
 
-            //}
+            }
             //foreach (var str in stringarray)
             //{
             //    Console.WriteLine(str);// another way to create the list
-            }
+           //}
 
 
             int[] numArray = new int[] { 10, 32, 32, 45, 54, 65, 234 };// creating a array of numbers
-            try
+            try// a try and catch method can be used like and if and else statment, if its not his then it would print out the error exception e.
             {
                 Console.WriteLine("now pick a number between 0 and 6 to get a number");// asking user for number between 0 and 6 anything else and it wont work
                 int index2 = Convert.ToInt32(Console.ReadLine());// to get user input
@@ -45,20 +49,31 @@ namespace Arrays
             stringList.Add("Howdy");//adding the word howdy it now has the index value 0
             stringList.Add("Hello");//adding the word hello it now has the index value 1
             stringList.Add("Hi");//adding the word hi it now has the index value 2
-            Console.WriteLine("Pick a number between 0 and 2 to get a greeting");// telling user to pick a number between 0 and 2, anthing else the program wont work
-            int index3 = Convert.ToInt32(Console.ReadLine());// getting user input
-
-            for (int y = 0; y < stringList.Count; y++)// the count pretty much means the same as length but its what is used for lists.
+            try
             {
+                Console.WriteLine("Pick a number between 0 and 2 to get a greeting");// telling user to pick a number between 0 and 2, anthing else the program wont work
+                int index3 = Convert.ToInt32(Console.ReadLine());// getting user input
+                Console.WriteLine(stringList[index3]);
+            }
 
-                if (index3 == y)
-                {
-                    Console.WriteLine(stringList[y]);
-                }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
 
 
-            }// can also use the try method used for the numarray for this it would be easier.
-        }
+
+            //for (int y = 0; y < stringList.Count; y++)// the count pretty much means the same as length but its what is used for lists.
+            //{
+
+            //    if (index3 == y)
+            //    {
+            //        Console.WriteLine(stringList[y]);
+            //    }
+
+
+        }// can also use the try method used for the numarray for this it would be easier.
+        
     }
 }
             
---
+++ Iteration/Iteration/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Iteration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int[] testScores = { 98, 99, 85, 70, 82, 34, 91, 90, 94 };
+
+            //for(int i = 0; i < testScores.Length; i++)
+            //{
+            //    if (testScores[i] > 85)
+            //    {
+            //        Console.WriteLine("Passing test score: " + testScores[i]);
+            //    }
+            //}
+            //Console.ReadLine();
+
+            string[] names = { "nuny", "yilmar", "chris", "jay" };
+            for (int j = 0; j < names.Length; j++)
+            {
+                if (names[j] == "nuny")
+                {
+                    Console.WriteLine(names[j]);
+                }
+            }
+
+                        
+        }
+    }
+}
