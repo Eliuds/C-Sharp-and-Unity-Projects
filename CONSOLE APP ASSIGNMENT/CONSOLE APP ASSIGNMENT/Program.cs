e1a1c1dab06eb40e6bda0ba0e225e37058df36f6
Eliuds <cmeliud2003@gmail.com>
2022-02-14 22:06:24 -0500

    asd

--- CONSOLE APP ASSIGNMENT/CONSOLE APP ASSIGNMENT/Program.cs
+++ CONSOLE APP ASSIGNMENT/CONSOLE APP ASSIGNMENT/Program.cs
@@ -7,18 +7,55 @@ namespace CONSOLE_APP_ASSIGNMENT
     {
         static void Main(string[] args)
         {
-            string[] stringarray = new string[] { "anto", "nuny", "jay", "yilmar" };
-            Console.WriteLine("Write something that will go to the end of every name.");
-            string user =Console.ReadLine();// this is getting a string input from the users
-            for (int j = 0; j < stringarray.Length; j++)
-            {
-                stringarray[j] += user;
-                Console.WriteLine(stringarray[j]);
-            }
-           
-           
-         
+            //string[] stringarray = new string[] { "anto", "nuny", "jay", "yilmar" };
+            //Console.WriteLine("Write something that will go to the end of every name.");
+            //string user =Console.ReadLine();// this is getting a string input from the users
+            //for (int j = 0; j < stringarray.Length; j++)
+            //{
+            //    stringarray[j] += user;
+            //    Console.WriteLine(stringarray[j]);
+            //}
 
+
+            int count = 0;
+
+            while (true)
+            {
+                Console.WriteLine("Count = {0}.", count);
+                count++;
+
+                
+
+                if (count > 15)
+                {
+                    break;
+                }
+
+            }
+
+
+
+
+
+           int stuff = 0;
+
+            while (true)
+            {
+                Console.WriteLine("Count = {0}.", stuff);
+                stuff++;
+
+
+                if (stuff < 30)
+                {
+                    break;
+                }
+
+                if(stuff <= 25)
+                {
+                    break;
+                }
+
+            }
 
 
             Console.ReadLine();
