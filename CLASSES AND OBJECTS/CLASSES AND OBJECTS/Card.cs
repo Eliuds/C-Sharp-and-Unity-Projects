4010b2e4155087bd6e0a5e91f226ed422a8265d9
Eliuds <cmeliud2003@gmail.com>
2022-02-15 22:08:12 -0500

    asd

---
+++ CLASSES AND OBJECTS/CLASSES AND OBJECTS/Card.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CLASSES_AND_OBJECTS
+{
+    class Card
+    {
+        public string Suit { get; set; }
+        public string Face { get; set; }
+    }
+}
