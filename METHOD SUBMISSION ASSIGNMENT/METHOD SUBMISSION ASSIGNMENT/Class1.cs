a0bd8292a20538671ae40d54ecc9abd5adb4a010
Eliuds <cmeliud2003@gmail.com>
2022-02-18 15:48:56 -0500

    asd

--- METHOD SUBMISSION ASSIGNMENT/METHOD SUBMISSION ASSIGNMENT/Class1.cs
+++ METHOD SUBMISSION ASSIGNMENT/METHOD SUBMISSION ASSIGNMENT/Class1.cs
@@ -4,7 +4,13 @@ using System.Text;
 
 namespace METHOD_SUBMISSION_ASSIGNMENT
 {
-    class Class1
+    public class Methods
     {
+
+        public int Method(int x, int y = 1)// to make it optional you have to give it a fallback number. If y is not given a number it will use that fallback number in this case 1.
+        {
+            return x + y;
+        }
+
     }
 }
--- METHOD SUBMISSION ASSIGNMENT/METHOD SUBMISSION ASSIGNMENT/Program.cs
+++ METHOD SUBMISSION ASSIGNMENT/METHOD SUBMISSION ASSIGNMENT/Program.cs
@@ -6,7 +6,18 @@ namespace METHOD_SUBMISSION_ASSIGNMENT
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Give me a number.");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("optionally you can give me a 2nd number if not it will use the number 1.");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            Methods obj1 = new Methods();
+            int result = obj1.Method(num, num2);
+            Console.WriteLine("The result is " + result);
+                
+
+            
         }
     }
 }
