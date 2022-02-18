0dd6eb80b069ef14bfe7c142dec426c07b6f2728
Eliuds <cmeliud2003@gmail.com>
2022-02-18 16:34:19 -0500

    asd

--- METHOD SUBMISSION ASSIGNMENT/METHOD SUBMISSION ASSIGNMENT/Program.cs
+++ METHOD SUBMISSION ASSIGNMENT/METHOD SUBMISSION ASSIGNMENT/Program.cs
@@ -10,14 +10,25 @@ namespace METHOD_SUBMISSION_ASSIGNMENT
             int num = Convert.ToInt32(Console.ReadLine());
 
             Console.WriteLine("optionally you can give me a 2nd number if not it will use the number 1.");
-            int num2 = Convert.ToInt32(Console.ReadLine());
+            try
+            {
+                int num2 = Convert.ToInt32(Console.ReadLine());
+                Methods obj1 = new Methods();
+                int result = obj1.Method(num, num2);
+                Console.WriteLine("The result is " + result);
+            }
 
-            Methods obj1 = new Methods();
-            int result = obj1.Method(num, num2);
-            Console.WriteLine("The result is " + result);
-                
+            catch
+            {
+                Methods obj1 = new Methods();
+                int result = obj1.Method(num);
+                Console.WriteLine("The result is " + result);
+            }
 
-            
+
+
+
+
         }
     }
 }
