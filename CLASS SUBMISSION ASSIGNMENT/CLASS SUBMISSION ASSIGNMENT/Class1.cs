eedc2de04f54a1bf32aa7b3b74df274b0a9f15db
Eliuds <cmeliud2003@gmail.com>
2022-02-18 23:07:24 -0500

    asd

--- CLASS SUBMISSION ASSIGNMENT/CLASS SUBMISSION ASSIGNMENT/Class1.cs
+++ CLASS SUBMISSION ASSIGNMENT/CLASS SUBMISSION ASSIGNMENT/Class1.cs
@@ -6,5 +6,28 @@ namespace CLASS_SUBMISSION_ASSIGNMENT
 {
     class Class1
     {
+        public void Divide(int x)// this is the method
+        {
+            x = x / 2;
+            Console.WriteLine(x);
+
+        }
+
+        public void Outint(out int x)
+        {
+            int temp = 2;
+            x = temp;
+        }
+
+        static int PlusMethod(int x, int y)
+        {
+            return x + y;
+        }
+
+        static string PlusMethod(string x, string y)
+        {
+            return x + y;
+        }
+
     }
 }
--- CLASS SUBMISSION ASSIGNMENT/CLASS SUBMISSION ASSIGNMENT/Program.cs
+++ CLASS SUBMISSION ASSIGNMENT/CLASS SUBMISSION ASSIGNMENT/Program.cs
@@ -6,7 +6,10 @@ namespace CLASS_SUBMISSION_ASSIGNMENT
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Give me a number to divide by 2.");
+            int user = Convert.ToInt32(Console.ReadLine());
+            Class1 obj1 = new Class1();
+            obj1.Divide(user);
         }
     }
 }
