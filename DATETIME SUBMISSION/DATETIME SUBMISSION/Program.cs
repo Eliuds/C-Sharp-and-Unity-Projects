1d766d57d9b7416b2d39e22b3a2d5670528907d3
Eliuds <cmeliud2003@gmail.com>
2022-06-05 17:15:37 -0400

    Read me

--- DATETIME SUBMISSION/DATETIME SUBMISSION/Program.cs
+++ DATETIME SUBMISSION/DATETIME SUBMISSION/Program.cs
@@ -7,13 +7,13 @@ namespace DATETIME_SUBMISSION
     {
         static void Main(string[] args)
         {
-            using (StreamWriter file = new StreamWriter(@"C:\Users\13218\log.txt", true))
+            using (StreamWriter file = new StreamWriter(@"C:\Users\13218\log.txt", true))// Choosing a path where the txt file will go
             {
 
                 file.WriteLine(DateTime.Now);
                 Console.WriteLine("Pick a number");
                 int Number = Convert.ToInt32(Console.ReadLine());
-                var Date1 = DateTime.Now;
+                var Date1 = DateTime.Now;// creating a var that holds DateTime.Now
                 var future = Date1.AddHours(Number);
                 Console.WriteLine(future);
             }
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # C-Sharp-and-Unity-Projects
 The project i will make in the tech academy's c# and unity course.
+i made allot of projects with c# i made a blackjack game where i used loops, paremiters and a ton of other things to be able to make it operate properly.
